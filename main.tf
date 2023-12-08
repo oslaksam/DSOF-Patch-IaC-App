199272cf10244f055109ed4e0bea249a13e5f41b
Samson Oslakov <samolak@dfds.com>
2023-12-08 11:37:31 +0100

    aws provider fix

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.67"
+    }
+  }
+}
+
 provider "aws" {
-  version                     = "~> 2.67"
   region                      = var.region
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
 }
-
 resource "aws_iam_account_password_policy" "strict" {
   minimum_password_length = 15
   require_lowercase_characters   = true
