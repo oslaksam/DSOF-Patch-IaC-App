596d4c6689d70199442c64895f1f149d4bea543b
Samson Oslakov <samolak@dfds.com>
2023-12-08 16:15:14 +0100

    Infra fix

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,5 +1,10 @@
-resource "aws_cloudtrail" "insecure-logging" {
-  name           = "cloudtrail-logging"
-  s3_bucket_name = "my-cloudtrail-bucket"
-  enable_logging = false
+resource "aws_cloudtrail" "secure_logging" {
+  name                          = "cloudtrail-logging"
+  s3_bucket_name                = "my-cloudtrail-bucket"
+  enable_logging                = true
+  cloud_watch_logs_group_arn    = "arn:aws:logs:REGION:ACCOUNT-ID:log-group:asd"
+  cloud_watch_logs_role_arn     = "arn:aws:iam::ACCOUNT-ID:role/asd"
+  enable_log_file_validation    = true
+  is_multi_region_trail         = true
+  kms_key_id                    = "arn:aws:kms:REGION:ACCOUNT-ID:key/asd"
 }
--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,19 +1,48 @@
-resource "aws_s3_bucket" "insecure-bucket" {
-  bucket = "insecure-bucket"
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "secure-bucket" // Replace with your primary bucket name
 }
 
-# resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_versioning" "secure_bucket_versioning" {
+  bucket = aws_s3_bucket.secure_bucket.id
 
-resource "aws_ebs_volume" "example" {
-  availability_zone = "us-east-1a"
-  size              = 20
-  encrypted         = false
-  tags = {
-    Name = "insecure"
+  versioning_configuration {
+    status = "Enabled"
+    mfa_delete = "Enabled"
   }
+}
+
+resource "aws_s3_bucket_logging" "secure_bucket_logging" {
+  bucket = aws_s3_bucket.secure_bucket.id
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+}
+
+resource "aws_s3_bucket_public_access_block" "secure_bucket" {
+  bucket                  = aws_s3_bucket.secure_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+// Logging Bucket Configuration
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-log-bucket" // Replace with your logging bucket name
+}
+
+resource "aws_s3_bucket_versioning" "log_bucket_versioning" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+    mfa_delete = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "log_bucket" {
+  bucket                  = aws_s3_bucket.log_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
